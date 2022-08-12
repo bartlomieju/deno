1dfa47883e642a5c4ca4748832fbd439d181dc03
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-08-12 02:00:24 +0200

    disable perm check, inject process global

--- ext/node/lib.rs
+++ ext/node/lib.rs
@@ -84,12 +84,13 @@ fn ensure_read_permission(
   state: &mut OpState,
   file_path: &Path,
 ) -> Result<(), AnyError> {
+  Ok(())
   // allow borrowing state mutably twice by taking the resolver
   // and then putting it back after
-  let resolver = state.take::<Rc<dyn DenoDirNpmResolver>>();
-  let result = resolver.ensure_read_permission(state, file_path);
-  state.put(resolver);
-  result
+  // let resolver = state.take::<Rc<dyn DenoDirNpmResolver>>();
+  // let result = resolver.ensure_read_permission(state, file_path);
+  // state.put(resolver);
+  // result
 }
 
 #[op]
